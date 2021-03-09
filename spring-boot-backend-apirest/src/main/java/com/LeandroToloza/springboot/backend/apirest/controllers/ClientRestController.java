cd2c2590eb5ea54420e529aa52f78561dca3ee69
LeanTolo <leandrotoloza8@gmail.com>
2021-03-09 11:19:28 -0300

    Api rest crud finished

--- spring-boot-backend-apirest/src/main/java/com/LeandroToloza/springboot/backend/apirest/controllers/ClientRestController.java
+++ spring-boot-backend-apirest/src/main/java/com/LeandroToloza/springboot/backend/apirest/controllers/ClientRestController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -20,7 +22,6 @@ import com.LeandroToloza.springboot.backend.apirest.models.services.IClientServi
 @RestController
 @RequestMapping("/api")
 public class ClientRestController {
-
 	
 	@Autowired
 	private IClientService clientService;
@@ -41,6 +42,23 @@ public class ClientRestController {
 	public Client create(@RequestBody Client client) {
 		//client.setCreatedDate(new Date());
 		return clientService.save(client);
+	}
+	
+	@PutMapping("/clients/{id}")
+	@ResponseStatus(HttpStatus.CREATED) //201
+	public Client update(@RequestBody Client client, @PathVariable Long id) {
+		Client actualCli = clientService.findById(id);
+		actualCli.setName(client.getName());
+		actualCli.setSurname(client.getSurname());
+		actualCli.setEmail(client.getEmail());
+		
+		return clientService.save(actualCli);
+	}
+	
+	@DeleteMapping("/clients/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT) //204
+	public void delete(@PathVariable Long id) {
+		clientService.delete(id);
 	}
 	
 }
