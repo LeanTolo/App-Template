e5a432ab470991151888969240943477307092b2
LeanTolo <leandrotoloza8@gmail.com>
2021-03-20 11:32:21 -0300

    Dialog//PopUp to Users for modifications

---
+++ Angular/src/app/components/dialogs/dialog-title/dialog-title.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-title',
+  templateUrl: './dialog-title.component.html',
+  styleUrls: ['./dialog-title.component.css']
+})
+export class DialogTitleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
